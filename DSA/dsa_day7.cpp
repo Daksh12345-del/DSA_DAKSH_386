190378663599a38614cc1c955aa89eded83bd12d
Daksh Singhal <psinghal651@gmail.com>
2025-10-22 17:24:16 +0530

    My Dsa till now

---
+++ DSA/dsa_day7.cpp
@@ -0,0 +1,13 @@
+/*Bitwise Operators
+  Bitwise & means multiply
+  Bitwise | means add (or)
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 4, b = 8;
+    cout << (a & b) << endl;
+    cout << (a | b) << endl;
+    return 0;
+}
